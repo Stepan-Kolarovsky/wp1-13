94017cb6996f2aaa0e708470d6c0d2ee75a63442
Stepan-Kolarovsky <60381824+Stepan-Kolarovsky@users.noreply.github.com>
2020-06-01 13:30:33 +0200

    Create index.php

---
+++ index.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    
+<form action="submit.php" method="post">
+    <label for="A">Číslo 1</label>
+    <input type="number" name="A" id="A">
+    <label for="B">Číslo 2</label>
+    <input type="number" name="B" id="B">
+    <input type="submit" value="Vypočítat">
+</form>
+</body>
+</html>
